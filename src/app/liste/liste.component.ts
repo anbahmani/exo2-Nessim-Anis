822fee066ffc610f9d1e3890f31e3cb614a1f3b5
Anis <anisbahmani@hotmail.fr>
2021-12-09 16:58:57 +0100

    add film link list

--- src/app/liste/liste.component.ts
+++ src/app/liste/liste.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Film } from '../modele/film';
 
 @Component({
   selector: 'app-liste',
@@ -6,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liste.component.scss']
 })
 export class ListeComponent implements OnInit {
+
+	public films:Film[] = [
+		{
+		  name: 'Les méchants',
+		  poster: '/assets/les_mechants.jpeg',
+		  link: 'Film 1'
+		},
+		{
+		  name: 'Creed',
+		  poster: '/assets/creed.jpeg',
+		  link: 'Film 2'
+		},
+		{
+		  name: 'Last duel',
+		  poster: '/assets/last_duel.jpeg',
+		  link: 'Film 3'
+		},
+		{
+		  name: 'Blood diamond',
+		  poster: '/assets/blood_diamond.jpeg',
+		  link: 'Film 4'
+		},
+		{
+		  name: '300',
+		  poster: '/assets/300.jpeg',
+		  link: 'Film 5'
+		}
+	  ];
 
   constructor() { }
 
